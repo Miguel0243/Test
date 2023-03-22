8192689bed5fa2243497a3681ceeaa2c6b6f4ecc
Miguel0243 <127975674+Miguel0243@users.noreply.github.com>
2023-03-22 09:50:34 -0500

    Enviar email con variales en codigo

--- EnviarCorreo.py
+++ EnviarCorreo.py
@@ -1,16 +1,37 @@
 import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-def send_email(subject,message,to):
-    to = ["oscar.lopez@onestopgroup.com"]
-    subject = "Hola Oscar"
-    message = "Prueba de cuerpo de correo"
-    try:
-        server = smtplib.SMTP('smtp.gmail.com',587)
-        server.starttls()
-        server.login('miguelh.espinosa@onestopgroup.com','Municipal22')
-        message = 'Subject:{}\n\n{}'.format(subject, message)
-        server.sendmail('miguelh.espinosa@onestopgroup.com','oscar.lopez@onestopgroup.com',message)
-        server.quit()
-        print("Success: Email sent!")
-    except:
-        print("Email failed to send.")
+def send_email():
+    sender_email = "miguelh.espinosa@onestopgroup.com"
+    receiver_email = "oscar.lopez@onestopgroup.com"
+    password = "Municipal22"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Asunto"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    html = """\
+    <html>
+        <body>
+            <p>Hola,<br>
+            Como estas?<br>
+            Espero que muy bien, que tengas un buen dia
+            </p>
+        </body>
+    </html>
+    """
+
+    html_part = MIMEText(html,"html")
+    message.attach(html_part)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com",465,context=context) as server:
+        server.login(sender_email,password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
+
+send_email()
