204773d64f76042bf0c5e9610e55e55f40c71f07
Miguel0243 <127975674+Miguel0243@users.noreply.github.com>
2023-03-23 15:04:23 -0500

    examples

--- AppTest/KivyTest.py
+++ AppTest/KivyTest.py
@@ -1,9 +1,23 @@
-import kivy
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
 
+class Ui(ScreenManager):
+    pass
 
-class FirstApp(kivy.app.App):
+class FirstApp(MDApp):
     def build(self):
-        return kivy.uix.label.Label(text = "Hola Mundo App")
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Teal"
+        Builder.load_file('design.kv')
+
+        return Ui()
     
-FirstApp().run()
+    def change_style(self,checked,value):
+        if value:
+            self.theme_cls.theme_style = "Dark"
+        else:
+            self.theme_cls.theme_style = "Light"
+
+if __name__ == "__KivyTest__":  
+    FirstApp().run()
